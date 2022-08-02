efcbb5d482f6f9786be3f2997b8310ccf842216a
Dwaine Jade <dwainejade@gmail.com>
2022-08-02 02:30:37 +0000

    Update App.js

---
+++ src/Hero/Hero.js
@@ -0,0 +1,14 @@
+import React from "react"
+import 'Hero.scss'
+
+export default function HeroSection(){
+  return(
+    <div className="hero-wrapper">
+        <h1>
+          Where You Are Lactation
+        </h1>
+        <img src="" alt="black person breastfeeding"/>
+      <button>Book Now</button>
+    </div>
+  )
+}
